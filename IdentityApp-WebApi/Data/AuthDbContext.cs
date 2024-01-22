d2d3ac51c477c4a75f2c66d05c8fe634317a92f8
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-01-22 12:16:12 +0100

    configer the authdbcontext

---
+++ IdentityApp-WebApi/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityApp_WebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp_WebApi.Data
+{
+    public class AuthDbContext:IdentityDbContext<User>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
