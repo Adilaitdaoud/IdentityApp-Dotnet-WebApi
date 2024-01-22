ac4bcc95ca423ad8377b825451d5d762f60672c5
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-01-22 14:32:41 +0100

    Json web toke configuration to create  Token after authentification #jwtConfiguration

---
+++ IdentityApp-WebApi/Services/JWTService.cs
@@ -0,0 +1,51 @@
+﻿using IdentityApp_WebApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityApp_WebApi.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _jwtKey;
+
+        public JWTService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+            //jwtKey is used for both encripting and descripting the JWT token 
+            _jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
+        }
+
+        public string CreteJWT(User user)
+        {
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName,user.FirstName),
+                new Claim(ClaimTypes.Surname,user.LastName)
+            };
+            var credentials = new SigningCredentials(_jwtKey, SecurityAlgorithms.HmacSha512Signature);
+            //description of our token 
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(userClaims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(_configuration["JWT:ExpiresInDays"])),
+                SigningCredentials = credentials,
+                Issuer = _configuration["JWT:Issuer"]
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(jwt);
+
+        }
+
+    }
+}
