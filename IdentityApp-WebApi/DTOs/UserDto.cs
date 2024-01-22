4073844ff6b8892e4c052b12f531e39a3b598d8d
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-01-22 15:08:12 +0100

    create DTOs folder and add userDto

---
+++ IdentityApp-WebApi/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityApp_WebApi.DTOs
+{
+    public class UserDto
+    {
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string JWT { get; set; }
+    }
+}
