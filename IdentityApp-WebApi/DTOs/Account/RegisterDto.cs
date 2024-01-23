cdf7bf3cb77fde0d8be8c159d63eca5f8670ba21
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-01-23 14:53:43 +0100

    correct the rugular expression

--- IdentityApp-WebApi/DTOs/Account/RegisterDto.cs
+++ IdentityApp-WebApi/DTOs/Account/RegisterDto.cs
@@ -11,7 +11,7 @@ namespace IdentityApp_WebApi.DTOs.Account
         [StringLength(15, MinimumLength = 3, ErrorMessage = "First name must be at least{3},and maximum {15} characters")]
         public string LastName { get; set; }
         [Required]
-        [RegularExpression("^\\w+@[a-zA-Z_]+?\\[a-zA-Z]{2,3}$",ErrorMessage ="Invalid email adress")]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",ErrorMessage ="Invalid email adress")]
         public string Email { get; set; }
         [Required]
         [StringLength(15, MinimumLength = 8, ErrorMessage = "First name must be at least{8},and maximum {15} characters")]
