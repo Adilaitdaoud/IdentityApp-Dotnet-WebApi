146ccda9f4671f1d404f42eae18723af2e12c811
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-01-23 21:02:54 +0100

    edit the Error Message of the StringLength in the RegisterDto

--- IdentityApp-WebApi/DTOs/Account/RegisterDto.cs
+++ IdentityApp-WebApi/DTOs/Account/RegisterDto.cs
@@ -5,16 +5,16 @@ namespace IdentityApp_WebApi.DTOs.Account
     public class RegisterDto
     {
         [Required]
-        [StringLength(15,MinimumLength =3,ErrorMessage ="First name must be at least{3},and maximum {15} characters")]
+        [StringLength(15,MinimumLength =3,ErrorMessage ="First name must be at least{2},and maximum {1} characters")]
         public string FirstName { get; set; }
         [Required]
-        [StringLength(15, MinimumLength = 3, ErrorMessage = "First name must be at least{3},and maximum {15} characters")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "First name must be at least{2},and maximum {1} characters")]
         public string LastName { get; set; }
         [Required]
         [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",ErrorMessage ="Invalid email adress")]
         public string Email { get; set; }
         [Required]
-        [StringLength(15, MinimumLength = 8, ErrorMessage = "First name must be at least{8},and maximum {15} characters")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "First name must be at least{2},and maximum {1} characters")]
         public string  Password { get; set; }
     }
 }
