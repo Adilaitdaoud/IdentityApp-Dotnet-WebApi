defcf3988e2298a135873e0fe7ff8314b1f2fa8c
Adil_Ait_Daoud <72772832+Adilaitdaoud@users.noreply.github.com>
2024-02-04 15:49:44 +0100

    create the confirm emailDto

---
+++ IdentityApp-WebApi/DTOs/Account/ConfirmEmailDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp_WebApi.DTOs.Account
+{
+    public class ConfirmEmailDto
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email adress")]
+        public string Email { get; set; }
+    }
+}
